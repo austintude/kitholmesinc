e99ef96e016b022076334453b9e7ca2c8c85a8d5
Daniel Bisett <daniel@austintatiousdesign.co>
2019-11-19 12:09:01 -0600

    v2.2.0 Blocks 1, 2 and 3 initial set up for mobile

---
+++ template-parts/content/oneThreeWrapper.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template part for oneThreeWrapper
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+$leftonethirdblock	= get_field('leftonethirdblock');
+$righttwothirdsblock	= get_field('righttwothirdsblock');
+$cta_form_url			= get_field('cta_form_url');
+$cta_text			= get_field('cta_text');
+$cta_loading_image			= get_field('cta_loading_image');
+?>
+	<div class="onethreewrapper">
+		<div class="leftOneThirdBlock">
+			<img src="<?php echo $leftonethirdblock['url']; ?>" alt="<?php echo $leftonethirdblock['alt']; ?>">
+
+		</div><!-- end .leftBlock -->
+		<div class="rightTwoThirdsBlock">
+			<div class="content">
+			<?php echo $righttwothirdsblock; ?>
+			</div><!-- end .content -->
+		</div><!-- end .rightBlock -->
+	</div> <!-- end .wrapper -->
+	<div class="ctaButton">
+	<amp-lightbox id="my-lightbox" layout="nodisplay">
+    <div class="lightbox" on="tap:my-lightbox.close" role="button" tabindex="0">
+
+      <amp-iframe width="350" height="300" layout="fixed"
+              sandbox="allow-scripts allow-same-origin allow-popups" frameborder="0"
+              src="<?php echo $cta_form_url; ?>">
+    <amp-img layout="fill"
+             src="<?php echo $cta_loading_image['url']; ?>"
+             placeholder></amp-img>
+  </amp-iframe>
+    </div>
+  </amp-lightbox>
+  <button id="estimate" class="btn btn-lg btn-danger" on="tap:my-lightbox" role="button" tabindex="0"><?php echo $cta_text; ?> »</button>
+
+	</div><!-- end .ctaButton -->
--- template-parts/header/custom-page-branding.php
+++ template-parts/header/custom-page-branding.php
@@ -10,12 +10,9 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-branding">
-<div id="mobileTopBar">
+	<div id="mobileTopBar">
 	&nbsp;
 </div>
 <?php the_custom_logo(); ?>
-<div class="titleTagWrapper">
-		<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-		<h2 class="tagline"><?php bloginfo( 'description' ); ?></h2>
-</div>
+
 </div><!-- .site-branding -->
