e2ee29fd8e70d21504131abdf3b5f0814cc0e50e
Daniel Bisett <daniel@austintatiousdesign.co>
2019-11-22 12:55:03 -0600

    v2.3.2 minor product page layout fixes

--- template-parts/content/oneThreeWrapper.php
+++ template-parts/content/oneThreeWrapper.php
@@ -24,18 +24,6 @@ $cta_loading_image			= get_field('cta_loading_image');
 		</div><!-- end .rightBlock -->
 	</div> <!-- end .wrapper -->
 	<div class="ctaButton">
-	<!-- <amp-lightbox id="my-lightbox" layout="nodisplay">
-    <div class="lightbox" on="tap:my-lightbox.close" role="button" tabindex="0">
-
-      <amp-iframe width="350" height="300" layout="fixed"
-              sandbox="allow-scripts allow-same-origin allow-popups" frameborder="0"
-              src="<?php echo $cta_form_url; ?>">
-    <amp-img layout="fill"
-             src="<?php echo $cta_loading_image['url']; ?>"
-             placeholder></amp-img>
-  </amp-iframe>
-    </div>
-  </amp-lightbox> -->
   <a href="<?php echo $cta_form_url; ?>">
   <button id="estimate" class="btn btn-lg btn-danger" role="button" tabindex="0"><?php echo $cta_text; ?> »</button>
 </a>
