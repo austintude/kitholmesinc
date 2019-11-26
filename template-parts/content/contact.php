d340d8fab2fd9c8c3c1199c850cdab045ca0e730
Daniel Bisett <daniel@austintatiousdesign.co>
2019-11-26 15:44:37 -0600

    v2.3.6 fixed contact kit page form feeds

--- template-parts/content/contact.php
+++ template-parts/content/contact.php
@@ -12,6 +12,7 @@ $top_block_phone_link				= get_field('top_block_phone_link');
 $mid_block_title				= get_field('mid_block_title');
 $mid_block_btn				= get_field('mid_block_btn');
 $mid_block_form_url				= get_field('mid_block_form_url');
+$btm_block_form_url				= get_field('btm_block_form_url');
 $btm_block_title				= get_field('btm_block_title');
 $btm_block_btn				= get_field('btm_block_btn');
 $contact_kit_title				= get_field('contact_kit_title');
@@ -74,9 +75,9 @@ $cta_loading_image				= get_field('cta_loading_image');
 	<h2>
 			<?php echo $contact_kit_title; ?>
 		</h2>
-		<button id="singing" class="btn btn-lg btn-danger" on="tap:singing-lightbox" role="button" tabindex="0"><?php echo $contact_kit_btn; ?> »</button>
-		<amp-lightbox id="singing-lightbox" layout="nodisplay">
-    <div class="lightbox" on="tap:singing-lightbox.close" role="button" tabindex="0">
+		<button id="singing" class="btn btn-lg btn-danger" on="tap:contact-lightbox" role="button" tabindex="0"><?php echo $contact_kit_btn; ?> »</button>
+		<amp-lightbox id="contact-lightbox" layout="nodisplay">
+    <div class="lightbox" on="tap:contact-lightbox.close" role="button" tabindex="0">
 
       <amp-iframe width="350" height="550" layout="intrinsic"
               sandbox="allow-scripts allow-same-origin allow-popups" frameborder="0"
